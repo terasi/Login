e4ff14eff253a26bd3d767ca9b0fd577f7bfd38e
terasi <hitokunn750@gmail.com>
2025-01-05 21:28:37 +0900

    LoginDAO sakusei made

---
+++ src/com/diworksdev/login/dao/LoginDAO.java
@@ -0,0 +1,5 @@
+package com.diworksdev.login.dao;
+
+public class LoginDAO {
+
+}
---
+++ src/com/diworksdev/login/util/DBConnector.java
@@ -0,0 +1,30 @@
+package com.diworksdev.login.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnector {
+    private static String driverName = "com.mysql.jdbc.Driver";
+    private static String url = "jdbc:mysql://localhost/logindb_terashima";
+
+    private static String user = "root";
+    private static String password = "mysql";
+
+    private Connection getConnection() {
+        Connection con = null;
+
+        try {
+            Class.forName(driverName);
+            con = (Connection) DriverManager.getConnection(url, user, password);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return con;
+    }
+
+}
